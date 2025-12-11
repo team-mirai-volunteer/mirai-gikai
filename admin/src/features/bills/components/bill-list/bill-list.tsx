e1967c0cecafcffb9adb35cb776b2ce2f19330bf
Shota Shimizu <tano.sukenyabu@gmail.com>
2025-12-11 09:29:57 +0900

    add preview for coming soon

--- admin/src/features/bills/components/bill-list/bill-list.tsx
+++ admin/src/features/bills/components/bill-list/bill-list.tsx
@@ -53,7 +53,8 @@ function BillCard({ bill }: { bill: Bill }) {
             billId={bill.id}
             publishStatus={bill.publish_status}
           />
-          {bill.publish_status === "draft" && (
+          {(bill.publish_status === "draft" ||
+            bill.publish_status === "coming_soon") && (
             <PreviewButton billId={bill.id} />
           )}
           {bill.publish_status === "published" && (
