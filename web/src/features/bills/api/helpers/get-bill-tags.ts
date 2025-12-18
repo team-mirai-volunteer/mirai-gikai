e1544cad548afe1c191b73fd4864b53da9e96726
Shota Shimizu <tano.sukenyabu@gmail.com>
2025-12-18 11:25:48 +0900

    remove export

--- web/src/features/bills/api/helpers/get-bill-tags.ts
+++ web/src/features/bills/api/helpers/get-bill-tags.ts
@@ -10,7 +10,7 @@ type BillTag = {
 /**
  * bill_idごとにタグをグループ化する
  */
-export function groupTagsByBillId(
+function groupTagsByBillId(
   billTags: BillTag[]
 ): Map<string, Array<{ id: string; label: string }>> {
   return billTags.reduce((acc, bt) => {
